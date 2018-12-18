1cd090008fe083c31b738b598857ab4c27e222b4
NjodA <alnasser.njod1@gmail.com>
2018-12-18 20:37:26 +0300

    turbolinks test

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -41,7 +41,7 @@
 //     });
 //     }
 
-$(document).ready(function() {
+$(document).on("turbolinks:load", function() {
   const answer1 = document.querySelector(".answer1");
   const answer2 = document.querySelector(".answer2");
   const answer3 = document.querySelector(".answer3");
